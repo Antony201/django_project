3b6dd910f921c47036ec2e53565cdf3d31a9e66a
Ricle <nebozhinskiy@gmail.com>
2020-08-31 16:04:15 +0300

    added post model

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path("", views.HomePageVies.as_view(), name="home"),
-    path("about/", views.AboutPageView.as_view(), name="about")
-]
