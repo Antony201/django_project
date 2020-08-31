94f24e2219bdb719339fddbdbb99d10d58a2426d
Ricle <nebozhinskiy@gmail.com>
2020-08-31 15:38:44 +0300

    added tests and urls

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.homePageView, name="home")
+    path("", views.HomePageVies.as_view(), name="home"),
+    path("about/", views.AboutPageView.as_view(), name="about")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse("Hello, world!")
+class HomePageVies(TemplateView):
+    template_name = "home.html"
+
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
